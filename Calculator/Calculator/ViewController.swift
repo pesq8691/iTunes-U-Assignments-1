ff5d35da9ade3b5a95903961dc3b4498576b18c4
pesq8691 <dpesqueda@csumb.edu>
2015-09-15 11:02:55 -0700

    Committing Project Files
    Project Files

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Daniel pesqueda on 9/15/15.
+//  Copyright (c) 2015 Daniel pesqueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
