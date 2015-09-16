a8bc9f187fd070b0f507aa21e4502a0a49b3bd37
pesq8691 <dpesqueda@csumb.edu>
2015-09-15 19:56:27 -0700

    Completed number input portion

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTypingANumber: Bool = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        display.text = display.text! + digit
+        if userIsInTheMiddleOfTypingANumber{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true 
+        }
     }
 
 
